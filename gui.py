ef4e43631c602d4b86295f162cf864a5544323af
60T-Main <guga.nadirashvili12@gmail.com>
2025-05-06 14:45:29 +0400

    added comments

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 from tkinter.filedialog import askdirectory
 import sys
 
-# Console redirect class
+# Console redirect class (to show console on GUI)
 class gui_console():
     def __init__(self, text_widget):
         self.text_widget = text_widget
@@ -30,28 +30,29 @@ def start_upload():
 window = Tk()
 window.title("YouTube Bulk Uploader")
 window.resizable(True, True)
-
+# icon
 icon = PhotoImage(file='youtube-123.png')  
 window.iconphoto(True, icon)
-
+# styles
 style = ttk.Style()
 style.configure('TButton', font=('Roboto', 10), padding=6)
 style.configure('TLabel', font=('Roboto', 12, 'bold'))
-
+# label
 label = ttk.Label(window, text="Select the Videos Folder")
 label.grid(row=0, column=0, columnspan=2, pady=(20, 10), padx=20)
-
+# read only text entry for showing folder path 
 path_var = StringVar()
 path_entry = ttk.Entry(window, textvariable=path_var, width=50, state='readonly')
 path_entry.grid(row=1, column=0, padx=(20, 10), pady=10)
-
+# browse button
 choose_btn = ttk.Button(window, text="Browse", command=choose_folder)  
 choose_btn.grid(row=1, column=1, padx=(0, 20), pady=10)
-
+# upload button
 upload_btn = ttk.Button(window, text="Upload", command=start_upload)  
 upload_btn.grid(row=2, column=0, columnspan=2, padx=(0, 20), pady=10)
 
-text_widget = Text(window, height=10, width=60)
+# creates GUI console
+text_widget = Text(window, height=10, width=60,)
 text_widget.grid(row=3, column=0, columnspan=2, padx=20, pady=(10, 20), sticky="nsew")
 
 window.grid_rowconfigure(3, weight=1)
@@ -59,7 +60,7 @@ window.grid_columnconfigure(0, weight=1)
 window.grid_columnconfigure(1, weight=1)
 
 
-# Redirect stdout/stderr BEFORE importing uploader
+# Redirect stdout/stderr for GUI console 
 sys.stdout = gui_console(text_widget)
 sys.stderr = gui_console(text_widget)
 
--- uploader.py
+++ uploader.py
@@ -8,33 +8,36 @@ import time
 
 SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 
-
+#  check credentials (login info) 
 def check_creds():
     creds = None
-
+    # if token exists, pull login info from it
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
-    # request login
+    # request login if token doesnt exist
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
+            # refreshes token if expired
             creds.refresh(google.auth.transport.requests.Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file("client_secrets.json", SCOPES)
             creds = flow.run_local_server(port=0)
         
-        # save credentials
+        # save credentials in token
         with open("token.json", "w") as token:
             token.write(creds.to_json())
     
     return creds
 
+#  begin upload process
 def uploader(creds, video_folder):
     youtube = build("youtube", "v3", credentials=creds)
-
+    # searches .mp4 files in selected folder
     videos = os.listdir(path=video_folder)
     video_files = [f for f in videos if f.endswith(".mp4")]
 
+    # uploading individual tracks in folder
     for video_file in video_files:
         try:
             track_name = os.path.splitext(video_file)[0]
@@ -44,6 +47,7 @@ def uploader(creds, video_folder):
             video_path = os.path.join(video_folder, video_file)
             media = MediaFileUpload(video_path, chunksize=-1, resumable=True)
 
+            # track details
             request = youtube.videos().insert(
                 part="snippet,status",
                 body={
@@ -61,6 +65,7 @@ def uploader(creds, video_folder):
             response = request.execute()
             print(f"Uploaded: {video_file}, Video ID: {response['id']}")
 
+        # exceptions
         except Exception as e:
             print(f"Failed to upload {video_file}")
             if 'uploadLimitExceeded' in str(e):
